ec25d9d2398c1cdfa59c791429ed9d71cff5c6f9
recobee <recobee@gmail.com>
2014-10-09 14:49:03 +0530

    Location added but errors

--- app/src/main/java/com/livingwordsolutions/technosoft/post_module_location/SplashScreen.java
+++ app/src/main/java/com/livingwordsolutions/technosoft/post_module_location/SplashScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ProgressBar;
 
 public class SplashScreen extends Activity {
 
@@ -14,7 +15,7 @@ public class SplashScreen extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-
+        ((ProgressBar) findViewById(R.id.progressBar3)).setIndeterminate(true);
 		new Handler().postDelayed(new Runnable() {
 
 			/*
---
+++ app/src/main/java/com/livingwordsolutions/technosoft/post_module_location/map_locate.java
@@ -0,0 +1,79 @@
+package com.livingwordsolutions.technosoft.post_module_location;
+
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class map_locate extends Fragment {
+
+    Context context;
+    Button getLocationButton;
+    TextView lat, lon;
+    EditText address;
+
+    public map_locate() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View result = inflater.inflate(R.layout.map_locate, container, false);
+
+        map_comp();
+        return result;
+    }
+
+    private void map_comp() {
+        getLocationButton = (Button) getActivity().findViewById(R.id.locationButton);
+        lat = (TextView)getActivity().findViewById(R.id.latText);
+        lon = (TextView) getActivity().findViewById(R.id.longText);
+        address = (EditText) getActivity().findViewById(R.id.address);
+        getLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(context, MapActivity.class);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == 1 && data != null) {
+            if(resultCode == getActivity().RESULT_OK) {
+                String lat = data.getStringExtra("lat");
+                String lon = data.getStringExtra("lon");
+                String address = data.getStringExtra("address");
+                this.lat.setText(lat);
+                this.lon.setText(lon);
+                this.address.setText(address);
+
+
+            }
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "Not Loaded.. Please try again..!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+}
