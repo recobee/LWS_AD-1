c61f1941ecb74202977ec4fb3eae34f6bb248c74
recobee <recobee@gmail.com>
2014-10-09 16:08:48 +0530

    still facing problem

--- app/src/main/java/com/livingwordsolutions/technosoft/post_module_location/map_locate.java
+++ app/src/main/java/com/livingwordsolutions/technosoft/post_module_location/map_locate.java
@@ -1,11 +1,9 @@
 package com.livingwordsolutions.technosoft.post_module_location;
 
 
-
-import android.content.Context;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,11 +15,10 @@ import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
- *
  */
 public class map_locate extends Fragment {
 
-    Context context;
+
     public Button getLocationButton;
     TextView lat, lon;
     EditText address;
@@ -35,31 +32,34 @@ public class map_locate extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View result = inflater.inflate(R.layout.map_locate, container, false);
+        final View result = inflater.inflate(R.layout.map_locate, container, false);
 
-        map_comp();
+            getLocationButton = (Button) getActivity().findViewById(R.id.locationButton);
+            lat = (TextView) result.findViewById(R.id.latText);
+            lon = (TextView) result.findViewById(R.id.longText);
+            address = (EditText) result.findViewById(R.id.address);
+            getLocationButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+                /*@Override
+                public void onClick(View view) {
+
+                    Intent intent = new Intent(getActivity(), MapActivity.class);
+                    startActivityForResult(intent, 1);
+                }*/
+            });
+
         return result;
     }
 
-    private void map_comp() {
-        getLocationButton = (Button) getActivity().findViewById(R.id.locationButton);
-        lat = (TextView)getActivity().findViewById(R.id.latText);
-        lon = (TextView) getActivity().findViewById(R.id.longText);
-        address = (EditText) getActivity().findViewById(R.id.address);
-        getLocationButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                Intent intent = new Intent(getActivity(), MapActivity.class);
-                startActivityForResult(intent, 1);
-            }
-        });
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == 1 && data != null) {
-            if(resultCode == getActivity().RESULT_OK) {
+        if (requestCode == 1 && data != null) {
+            if (resultCode == getActivity().RESULT_OK) {
                 String lat = data.getStringExtra("lat");
                 String lon = data.getStringExtra("lon");
                 String address = data.getStringExtra("address");
@@ -73,7 +73,6 @@ public class map_locate extends Fragment {
             Toast.makeText(getActivity().getApplicationContext(), "Not Loaded.. Please try again..!", Toast.LENGTH_SHORT).show();
         }
     }
-
 
 
 }
